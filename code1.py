d050e77b57ed20f1512fbe1b5fd87e5f11c7b56e
JunbokYang <104674780+JunbokYang@users.noreply.github.com>
2022-05-23 12:30:15 +0900

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,13 @@
+print("5개의 점수를 입력하세요.")
+integer(1)=int(input)("정수 1의 값을 입력하시오.:")
+integer(2)=int(input)("정수 2의 값을 입력하시오.:")
+integer(3)=int(input)("정수 3의 값을 입력하시오.:")
+integer(4)=int(input)("정수 4의 값을 입력하시오.:")
+integer(5)=int(input)("정수 5의 값을 입력하시오.:")
+
+Sum = integer1+integer2+integer3+integer4+integer5
+
+Total = Sum / 5
+
+print(Sum)
+print(Total)
---
+++ code3.py
@@ -0,0 +1,4 @@
+pat_rate = 9160
+h_work = int(input)("일할 시간 입력: ")
+mon_pay + h_work * pay_rate
+print(mon_pay)
